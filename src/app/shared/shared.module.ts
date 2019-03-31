8a4e655048f0c1a8e13c925584e0505c60ca7620
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-01 00:27:46 +0100

    real-world: created shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  declarations: [],
+  providers: [],
+})
+export class SharedModule { }
