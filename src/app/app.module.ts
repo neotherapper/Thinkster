5002fd3bb5729c4927788856a02bd06f046acc9c
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-01 00:29:05 +0100

    real-world: added index.ts to layout and shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
