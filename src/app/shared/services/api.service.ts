9e6a776bb6fe6c441e6a450f3cfe715e1dfb6b18
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-01 03:38:52 +0100

    real-world:  fixed api service handling error bug

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -20,19 +20,8 @@ export class ApiService {
   }
 
   private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-    }
     // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
+    return throwError(error.error);
   }
 
   post(path: string, body: object = {}): Observable<any> {
