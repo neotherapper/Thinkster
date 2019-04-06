1e8a58b3938396f92624ea4acbaeabf09a6cfc23
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-06 17:17:32 +0100

    real-world: fixed auth-guard canActivate method

--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 import { UserService } from './user.service';
-import { take } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
   constructor(
+    private router: Router,
     private userService: UserService
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.userService.isAuthenticated.pipe(take[1]);
+    return this.userService.isAuthenticated;
   }
 }
