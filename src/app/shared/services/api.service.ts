da4cb15a180882080bc4a031a6570869a6865187
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-04-07 14:17:06 +0100

    real-world: using relative path so not to break stackbllitz

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../../environments/environment';
 import { JwtService } from './jwt.service';
 
 @Injectable()
